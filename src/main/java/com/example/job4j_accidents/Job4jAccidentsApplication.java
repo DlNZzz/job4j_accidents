a191ff435897bc310aeb8bd689b0202c1e3c4d68
Dinz <kostan-82@yandex.ru>
2022-09-17 14:36:07 +0300

    Initial commit

---
+++ src/main/java/com/example/job4j_accidents/Job4jAccidentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.job4j_accidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jAccidentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jAccidentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/job4j_accidents/Job4jAccidentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job4j_accidents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jAccidentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
