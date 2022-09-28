8e870ca5d754072860eb55156de58d0c6066257e
Dinz <kostan-82@yandex.ru>
2022-09-29 01:49:48 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/com/example/job4j_accidents/Job4jAccidentsApplication.java
+++ src/main/java/com/example/job4j_accidents/Job4jAccidentsApplication.java
@@ -8,6 +8,7 @@ public class Job4jAccidentsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Job4jAccidentsApplication.class, args);
+        System.out.println("Go to http://localhost:8080");
     }
 
 }
--- src/main/java/ru/job4j/controller/IndexController.java
+++ src/main/java/ru/job4j/controller/IndexController.java
@@ -1,4 +1,14 @@
 package ru.job4j.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("user", "Petr Arsentev");
+        return "index";
+    }
 }
